6493854f0e52b8fb80ba68759a72692e36cefe81
Rivishan Sandeep <87434970+RiviMAX@users.noreply.github.com>
2023-02-06 21:37:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['94789864930'] //ur owner number
+global.ownername = "😎𝑹𝒊𝒗𝒊𝒔𝒉𝒂𝒏🖤" //ur owner name
+global.ytname = "YT: SL Deep Remix" //ur yt chanel name
+global.socialm = "GitHub: rivishan_max" //ur github or insta name
+global.location = "Srilanka, Western, Gampaha" //ur location
 
 //bot bomdy 
-global.ownernomer = "916909137213" //ur number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot MD V6' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.ownernomer = "94789864930" //ur number
+global.premium = ['94764306687'] //ur premium number
+global.botname = '☺️Tharu Bot V2💕' //ur bot name
+global.linkz = "https://www.youtube.com/channel/UCKHVCIdKkhWaOFUygbQwK9Q" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.youtube.com/channel/UCKHVCIdKkhWaOFUygbQwK9Q" //ur website to be displayed
+global.botscript = 'http://wa.me/94789864930' //script link
+global.themeemoji = "🥰" //ur theme emoji
+global.packname = "☺️Tharu Bot V2💕" //ur sticker watermark packname
+global.author = "☺️Tharu Bot V2💕\n\n\nA whatsapp bot developed by\n😎𝑹𝒊𝒗𝒊𝒔𝒉𝒂𝒏🖤\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +94789864930" //ur sticker watermark author
+global.wm = "☺𝐓𝐡𝐚𝐫𝐮 𝐁𝐨𝐭 𝐓𝐞𝐚𝐦💞." //ur watermark
 
 // Other
 global.sessionName = 'session'
